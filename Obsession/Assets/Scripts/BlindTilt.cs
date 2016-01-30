9fa154bc75e9e38a7cdd9cd1e90b758ac687333c
Jonathon Shields <jonathonshields@gmail.com>
2016-01-31 09:21:37 +1300

    added audio

---
+++ Obsession/Assets/Scripts/BlindTilt.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlindTilt : MonoBehaviour {
+
+	public int degreesOfRotation;
+
+	// Use this for initialization
+	void Start () {
+//		GameObject[] blinds = gameObject.getC<GameObject>();
+//		foreach (GameObject blind in blinds) {
+//			blind.transform.eulerAngles =  new Vector3(0, degreesOfRotation, 0);
+//		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		GameObject[] blinds = gameObject.GetComponentsInChildren<GameObject>();
+//		foreach (GameObject blind in blinds) {
+//			blind.transform.eulerAngles =  new Vector3(0, degreesOfRotation, 0);
+//		}
+		foreach (Transform child in transform)
+		{
+			child.transform.eulerAngles =  new Vector3(0, degreesOfRotation, 0);
+			//child is your child transform
+		}
+	}
+}
---
+++ Obsession/Assets/Scripts/Highlight.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Highlight : MonoBehaviour {
+	private GameObject player;
+
+	public bool isBed;
+	public string levelToLoadNext;
+
+	public float objectViewDistance;
+	public float pauseTime = 5;
+	private bool HasClickedOnce = false;
+
+	public AudioClip narrationClip;
+	private AudioSource narrationSource;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+		narrationSource = gameObject.AddComponent<AudioSource> ();
+		narrationSource.clip = narrationClip;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseOver(){
+		if (Input.GetMouseButtonDown (0)) {
+			if (isBed) {
+				Application.LoadLevel (levelToLoadNext);
+			} else {
+				if (!HasClickedOnce) {
+					player.GetComponent<ZoomEffect> ().pauseTime = pauseTime;
+					HasClickedOnce = true;
+
+					narrationSource.Play ();
+				} else {
+					player.GetComponent<ZoomEffect> ().pauseTime = 0;
+				}
+			}
+		}
+
+		player.GetComponent<ZoomEffect> ().target.x = gameObject.transform.position.x;
+		player.GetComponent<ZoomEffect> ().target.z = gameObject.transform.position.z;
+		player.GetComponent<ZoomEffect> ().targetdistance = objectViewDistance;
+		player.GetComponent<ZoomEffect> ().CanZoom = true;
+
+
+		Behaviour halo = (Behaviour)GetComponent("Halo");
+
+		halo.enabled = true; // false
+
+	}
+	void OnMouseExit(){
+		Behaviour halo = (Behaviour)GetComponent("Halo");
+
+		halo.enabled = false; // false
+
+		player.GetComponent<ZoomEffect> ().CanZoom = false;
+
+	}
+
+
+}
