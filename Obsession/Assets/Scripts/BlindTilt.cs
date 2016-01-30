bbd7fbf4f1ab5568829c6dc9cf74298abebfc020
Jonathon Shields <jonathonshields@gmail.com>
2016-01-31 11:52:55 +1300

    Added Sarah voice

--- Obsession/Assets/Scripts/BlindTilt.cs
+++ Obsession/Assets/Scripts/BlindTilt.cs
@@ -7,22 +7,15 @@ public class BlindTilt : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-//		GameObject[] blinds = gameObject.getC<GameObject>();
-//		foreach (GameObject blind in blinds) {
-//			blind.transform.eulerAngles =  new Vector3(0, degreesOfRotation, 0);
-//		}
+				foreach (Transform child in transform)
+		{
+			child.transform.eulerAngles =  new Vector3(degreesOfRotation, 0, 0);
+			//child is your child transform
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		GameObject[] blinds = gameObject.GetComponentsInChildren<GameObject>();
-//		foreach (GameObject blind in blinds) {
-//			blind.transform.eulerAngles =  new Vector3(0, degreesOfRotation, 0);
-//		}
-		foreach (Transform child in transform)
-		{
-			child.transform.eulerAngles =  new Vector3(0, degreesOfRotation, 0);
-			//child is your child transform
-		}
+//		
 	}
 }
