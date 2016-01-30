a4dfdc73924c51d5ffd9d98750c2f236f3950539
Jonathon Shields <jonathonshields@gmail.com>
2016-01-30 13:16:00 +1300

    Initial Commit

---
+++ Obsession/Assets/Highlight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Highlight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnMouseOver(){
+		Behaviour halo = (Behaviour)GetComponent("Halo");
+		
+		halo.enabled = true; // false
+
+	}
+	void OnMouseExit(){
+		Behaviour halo = (Behaviour)GetComponent("Halo");
+		
+		halo.enabled = false; // false
+
+	}
+
+}
